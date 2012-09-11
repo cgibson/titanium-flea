aae54dfc20154e609bbf948e61be755f9e2ec133
Chris Gibson <cgibson@mrvoxel.com>
2012-09-11 00:00:05 -0700

    improved publishing, though added error when updating posts.  improved CSS style for main page

--- python/tiflea/publish.py
+++ python/tiflea/publish.py
@@ -14,32 +14,47 @@ import couchdb
 from post import Post
 import settings
 
-def publish(title, mdtext, html, category, author="Chris", tags=[], timestamp=datetime.datetime.now(), preview=False):
+def publish(post_meta):
 
 
-    doc_id = Post.url_friendly_text(title)
+    doc_id = Post.url_friendly_text(post_meta["title"])
+    
+    # Open the database
     couch = couchdb.Server()
     db = couch["mrvoxel_blog"]
+    
+    # Load the database settings
     blog_settings = settings.loadSettings(db)
     
-    if not category in blog_settings["blog_categories"]:
-        raise ValueError("No such category: %s" % category)
+    # Check to see if we have a valid category
+    if not post_meta["category"] in blog_settings["blog_categories"]:
+        raise ValueError("No such category: %s" % post_meta["category"])
     
     print "checking for [%s]" % doc_id
     
+    # Load the post (if it exists in our database)
     post = Post.load(db, doc_id)
     
+    # If it exists, warn the user
     if post:
         raw = raw_input("This will replace an existing post of the same title...\nContinue? (y/N)")
         
         # if the existing post is published but we're trying to preview
-        if (post["published"] == True) and preview:
+        if (post["published"] == False) and post_meta["published"]:
             raise ValueError("Cannot yet preview posts that are already published")
         
         if raw != "y":
             print "Canceling publish."
             return None
         else:
+            for k, v in post_meta.iteritems():
+                if k not in ["published", "title"]:
+                    print k
+                    post[k] = v
+            
+            print post
+            
+            """
             post.markdown = mdtext
             post.html = html
             post.author = author
@@ -48,11 +63,12 @@ def publish(title, mdtext, html, category, author="Chris", tags=[], timestamp=da
             post.title = title
             post.tags = tags
             post.category = category
+            """
             
             post.store(db)
     else:
         
-        post = Post.create(title=title, markdown=mdtext, html=html, category=category, author=author, timestamp=timestamp, published=not preview)
+        post = Post.create(**post_meta)
         post.store(db)
         
     return post._id
@@ -62,31 +78,70 @@ if __name__ == '__main__':
     
     parser = argparse.ArgumentParser(description="Publishes posts to the current TiFl blog")
     
-    parser.add_argument("title")
     parser.add_argument("post_file", type=argparse.FileType('r'))
-    parser.add_argument("category")
-    parser.add_argument("-author", default="Chris")
-    parser.add_argument("-created", default=datetime.datetime.now())
-    parser.add_argument("-tags", nargs="*", default=[])
+    parser.add_argument("-title", default=None)
+    parser.add_argument("-category", default=None)
+    parser.add_argument("-author", default=None)
+    parser.add_argument("-date", default=None)
+    parser.add_argument("-tags", nargs="*", default=None)
     parser.add_argument("-preview", action="store_true", default=False)
     
+    # Parse the arguments
     opts = parser.parse_args()
     
-    title = opts.title
-    mdtext = opts.post_file.read()
+    # Create the post argument dictionary
+    post_meta = {}
     
-    # Convert to unicode to avoid silly errors with encoding
-    # unicode_content = text.encode('utf8', 'ignore')
-    html = markdown.markdown(mdtext, ['codehilite', mdx_mathjax.makeExtension()])
-    author = opts.author
-    tags = opts.tags
-    preview = opts.preview
-    timestamp = datetime.datetime.strptime(opts.created, "%m/%d/%Y")
-    category = opts.category
+    # Create the markdown file
+    md = markdown.Markdown(extensions=['codehilite','meta', mdx_mathjax.makeExtension()])
     
+    # Load the markdown text and create the html from it
+    post_meta["markdown"] = opts.post_file.read()
     opts.post_file.close()
+    post_meta["html"] = md.convert(post_meta["markdown"])
     
-    doc_id = publish(title, mdtext, html, category, author=author, tags=tags, timestamp=timestamp, preview=preview)
+    print md.Meta
+    # Load file-based metadata
+    for k, v in md.Meta.iteritems():
+        if k in ["markdown", "html"]:
+            continue
+        
+        # Necessary conversions for arrays
+        if k == "tags":
+            tags = []
+            for line in v:
+                tags += line.split(" ")
+            v = tags
+        else:
+            v = v[0]
+        
+        # Other random conversions
+        if k == "date":
+            k = "timestamp"
+            v = datetime.datetime.strptime(v, "%m/%d/%Y")
+        
+        # Save the data
+        post_meta[k] = v
+    
+    # Now overwrite any metadata if explicitely listed in the arguments
+    if opts.title:
+        post_meta["title"] = opts.title
+    
+    if opts.author:
+        post_meta["author"] = opts.author
+    
+    if opts.tags:
+        post_meta["tags"] = opts.tags
+    
+    if opts.date:
+        post_meta["timestamp"] = datetime.datetime.strptime(opts.date, "%m/%d/%Y")
+    
+    if opts.category:
+        post_meta["category"] = opts.category
+    
+    post_meta["published"] = not opts.preview
+    
+    doc_id = publish(post_meta)
     
     #if preview and doc_id:
     #    url = "%s/p/%s" % (s.SITE_BASE, doc_id)
